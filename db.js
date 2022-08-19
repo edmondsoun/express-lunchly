7aef2f406e5a0900fd8e3db208bc842a15517ff0
Edmond Soun <edmond.soun@gmail.com>
2022-08-19 11:15:48 -0700

    added windows line to db

--- db.js
+++ db.js
@@ -4,9 +4,16 @@
 
 const { Client } = require("pg");
 
+/** MAC EDITION */
+// const DB_URI = process.env.NODE_ENV === "test"
+//     ? "postgresql:///lunchly_test"
+//     : "postgresql:///lunchly";
+
+/** WINDOWS EDITION */
 const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgresql:///lunchly_test"
-    : "postgresql:///lunchly";
+  ? "postgresql://esoun:esoun@localhost/lunchly_test" 
+  : "postgresql://esoun:esoun@localhost/lunchly";
+
 
 let db = new Client({
   connectionString: DB_URI,
